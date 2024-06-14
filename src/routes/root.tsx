63822935b8b26d16698eff853e888a342cef61b0
Ayothegod <heyayomideadebisi@gmail.com>
2024-06-14 09:36:28 +0100

    debug: pipeops error-retry remove shadcn p

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,7 @@
-import { Button } from "../components/ui/button";
 
 export default function Root() {
   return <div>
      <h1 className="text-3xl font-bold underline">Hello world!</h1>
      <p>Pipeops</p>
-     <Button>Click Me</Button>
   </div>
 }
