0a7e211eaf9ec9ec34b4721a5848b3cf23937685
Ayothegod <heyayomideadebisi@gmail.com>
2024-06-14 09:55:01 +0100

    task: add basic routes

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,9 @@
+
+export default function RootLayout({ children }: any) {
+    return (
+      <main className="font-inter">
+        {children}
+      </main>
+    );
+  }
+  
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Root from './routes/root.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root.tsx";
+import RootLayout from "./layouts/RootLayout.tsx";
+import MainLayout from "./layouts/MainLayout.tsx";
+import Dashboard from "./routes/Dashboard.tsx";
+import Register from "./routes/register.tsx";
+import Login from "./routes/login.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-)
+    <RootLayout>
+      <RouterProvider router={router} />
+    </RootLayout>
+  </React.StrictMode>
+);
