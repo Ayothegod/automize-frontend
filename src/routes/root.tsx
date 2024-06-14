e227910542f82b632aed05ff24d146dc5d900965
Ayothegod <heyayomideadebisi@gmail.com>
2024-06-14 09:30:40 +0100

    debug: pipeops error

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Root() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <div>
+     <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     <Button>Click Me</Button>
+  </div>
 }
