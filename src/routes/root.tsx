f90731a3b3d61e8473650fc9c88b51b39205b034
Ayothegod <heyayomideadebisi@gmail.com>
2024-06-14 09:38:31 +0100

    debug: now works

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,11 @@
+import Header from "@/components/build/Header";
 
 export default function Root() {
-  return <div>
-     <h1 className="text-3xl font-bold underline">Hello world!</h1>
-     <p>Pipeops</p>
-  </div>
+  return (
+    <div>
+      <Header/>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>Pipeops</p>
+    </div>
+  );
 }
