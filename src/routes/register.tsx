f8e46eaf17add9c67872dcea370306f7a14689e0
Ayothegod <heyayomideadebisi@gmail.com>
2024-06-15 19:22:09 +0100

    task: add form packages

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,8 +1,13 @@
-// import { registerSchema } from "@/lib/schema/authSchema"
+// import { registerSchema } from "@/lib/schema/authSchema";
+
+// type RegisterSchemaType = z.infer<typeof registerSchema>;
+// import { useForm } from "react-hook-form";
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm<RegisterSchemaType>({ resolver: zodResolver(registerSchema) });
+  return <div>Register</div>;   
 }
- 
