aff9d5e85d40f94028fa453b40113c6260b2558c
Ayothegod <heyayomideadebisi@gmail.com>
2024-06-15 18:58:36 +0100

    task: define layput

---
+++ src/lib/data.ts
@@ -0,0 +1,13 @@
+export const sidebarData = [
+  { id: 1, title: "Dashboard", url: "/dashboard", iconType: "LayoutDashboard" },
+  { id: 2, title: "Messages", url: "/messages", iconType: "MessageCircle" },
+  { id: 3, title: "Publishing", url: "/Publishing", iconType: "SquarePen" },
+  { id: 4, title: "Analytics", url: "/Analytics", iconType: "BarChartBig" },
+  {
+    id: 5,
+    title: "Collaboration",
+    url: "/Collaboration",
+    iconType: "LibraryBig",
+  },
+  { id: 6, title: "Accounts", url: "/Accounts", iconType: "Users" },
+];
